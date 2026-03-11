3790aa8e73cd272d697492ecca8d2d5a7905c23b
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2026-03-11 17:00:10 +0100

    added the first part of the project

--- src/main.c
+++ src/main.c
@@ -1,72 +1,11 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <getopt.h>
-#include <stdlib.h>
+#include "kv.h"
 
-#include "common.h"
-#include "file.h"
-#include "parse.h"
+int main(int argc, char* argv[]){
 
+    kv_t* table = kv_init(3);
+    printf("%p\n", table);
 
-void print_usage(char *argv[]) {
-    printf("Usage: %s -n -f <database file>\n", argv[0]);
-    printf("\t -n  -  Creat new database file\n");
-    printf("\t -f  -  (required) path to database file\n");
-    return;
-}
-
-
-int main(int argc, char *argv[]) { 
-	
-    char* filepath = NULL;
-    bool newfile = false;
-    int c;
-    int dbfd = -1;         // database filedescriptor
-
-    while ((c = getopt(argc, argv, "nf:")) != -1) {
-        switch (c) {
-            case 'n':
-                newfile = true;
-                break;
-            
-            case 'f':
-                filepath = optarg;
-                break;
-            
-            case '?':
-                printf("Unknown option -%c\n", c);
-                break;
-            
-            default:
-                return -1;
-        }
-    }
-
-    if(filepath == NULL){
-        printf("Filepath is a required argument!\n");
-        print_usage(argv);
-        return 0;
-    }
-
-
-    if(newfile){
-        dbfd = create_db_file(filepath);
-        if(dbfd == STATUS_ERROR){
-            printf("Unable to create database file!\n");
-            return -1;
-        }
-    } else {
-        dbfd = open_db_file(filepath);
-        if(dbfd == STATUS_ERROR){
-            printf("Unable to create database file!\n");
-            return -1;
-        }
-    }
-
-
-    printf("Newfile: %d\n", newfile);
-    printf("Filepath: %s\n", filepath);
 
     return 0;
-
 }
